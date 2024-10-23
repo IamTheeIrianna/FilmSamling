e1baf655bdf9be4c439dbc5563201f1e72293606
mads61q5 <mads61q5@stud.kea.dk>
2024-10-23 10:57:13 +0200

    Movie klasse

--- src/Movie.java
+++ src/Movie.java
@@ -1,10 +1,43 @@
 public class Movie {
     private String title;
     private String director;
-    private String yearCreated;
+    private int yearCreated;
     private String isInColor;
     private String lengthInMinutes;
     private String genre;
-}
 
+
+    public Movie(String title, String director, int yearCreated, String isInColor, String lengthInMinutes, String genre) {
+        this.title = title;
+        this.director = director;
+        this.yearCreated = yearCreated;
+        this.isInColor = isInColor;
+        this.lengthInMinutes = lengthInMinutes;
+        this.genre = genre;
+    }
+
+    public int getYearCreated() {
+        return yearCreated;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getIsInColor() {
+        return isInColor;
+    }
+
+    public String getLengthInMinutes() {
+        return lengthInMinutes;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
 
