5e3c7171a5a408949c6edef63bedf8bc39fba450
haiq0001 <haiq0001@stud.kea.dk>
2024-10-23 10:55:29 +0200

    addMovie tilføjet

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        MovieCollection movieCollection = new MovieCollection();
 
        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the title of your movie: ");
         String title = scanner.nextLine();
+
 
         System.out.print("Enter the director of the movie: ");
         String director = scanner.nextLine();
