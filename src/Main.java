961fa1e447360d741d54dd49e8b2dbf730c2a0f3
haiq0001 <haiq0001@stud.kea.dk>
2024-10-23 10:18:52 +0200

    lenght scanner

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,18 @@ public class Main {
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the title of your movie");
+        System.out.print("Enter the title of your movie: ");
         String title = scanner.nextLine();
 
-        System.out.println("Enter the director of the movie");
+        System.out.print("Enter the director of the movie: ");
         String director = scanner.nextLine();
 
-        System.out.println("Enter the year the movie was made");
+        System.out.print("Enter the year the movie was made: ");
         String year = scanner.nextLine();
+
+        System.out.print("Enter the lenght of your movie: ");
+        String lenght = scanner.nextLine();
+
+
     }
 }
