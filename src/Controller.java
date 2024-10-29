e95a31d02382338744f146d2ed0cb331ff4bcec4
daniellaisabella <dato0001@stud.kea.dk>
2024-10-29 15:51:13 +0100

    refaktorering af edit

--- src/Controller.java
+++ src/Controller.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
 public class Controller {
 
     private MovieCollection movieCollection;
     private Movie movie;
 
     public Controller() {
-        movieCollection = new MovieCollection();
+    movieCollection = new MovieCollection(new ArrayList<>());
+    movie = new Movie("Default Title", "Default Director", 1900, true, 60, "Default Genre");
+}
+   /* public Controller() {
+        movieCollection = new MovieCollection(new ArrayList<>());
         movie = new Movie(movie.getTitle(), movie.getDirector(), movie.getYearCreated(), movie.getIsInColor(), movie.getLengthInMinutes(), movie.getGenre());
-    }
+    }*/
 
     public MovieCollection getMovieCollection() {
         return movieCollection;
