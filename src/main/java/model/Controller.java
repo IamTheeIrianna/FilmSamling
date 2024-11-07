83662151efe80eb05b7f367f2c52492bf64ab512
daniellaisabella <dato0001@stud.kea.dk>
2024-11-07 14:11:48 +0100

    Update Controller.java

--- src/main/java/model/Controller.java
+++ src/main/java/model/Controller.java
@@ -32,7 +32,42 @@ public class Controller {
 
 
 }
+/*
+package model;
 
+import datasource.Filehandler;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+public class Controller {
+
+    private MovieCollection movieCollection;
+//    private Movie movie;
+    private Filehandler filehandler;
+
+    public Controller() {
+    movieCollection = new MovieCollection(new ArrayList<>());
+//    movie = new Movie("Default Title", "Default Director", 1900, true, 60, "Default Genre");
+    filehandler = new Filehandler();
+}
+
+
+
+    public MovieCollection getMovieCollection() {
+        return movieCollection;
+    }
+
+     public void loadMovies() throws FileNotFoundException {
+        filehandler.loadMovies();
+    }
+
+    public void saveMovies() throws FileNotFoundException {
+        filehandler.saveMovie(new ArrayList<>());
+    }
+
+
+}
+*/
 
 
 
