f40a47050f6429bd1b659fade60b8871cac091fe
mads61q5 <mads61q5@stud.kea.dk>
2024-11-07 13:43:14 +0100

    Update Controller.java

--- src/main/java/model/Controller.java
+++ src/main/java/model/Controller.java
@@ -1,10 +1,14 @@
 package model;
 
+import datasource.Filehandler;
+
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 public class Controller {
 
     private MovieCollection movieCollection;
     private Movie movie;
+    private Filehandler filehandler;
 
     public Controller() {
     movieCollection = new MovieCollection(new ArrayList<>());
@@ -13,6 +17,14 @@ public class Controller {
 
     public MovieCollection getMovieCollection() {
         return movieCollection;
+    }
+
+     public void loadMovies() throws FileNotFoundException {
+        filehandler.loadMovies();
+    }
+
+    public void saveMovies() throws FileNotFoundException {
+        filehandler.saveMovie(new ArrayList<>());
     }
 
 
